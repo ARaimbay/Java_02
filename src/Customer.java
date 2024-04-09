3f92b7c74552be964cc8065dd754a424e1643011
Aigerim Raimbay <raimbay89@gmail.com>
2024-04-08 21:53:22 -0500

    new project

---
+++ src/Customer.java
@@ -0,0 +1,12 @@
+public class Customer {
+    public Customer() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is the list of customers: ");
+        String type;
+        String name;
+        String address;
+
+    }
+}
